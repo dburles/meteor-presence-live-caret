292ce5aed0dcbe9bc78c9dfccbff81dfb264ad46
David Burles <dburles@gmail.com>
2014-05-22 14:53:55 +1000

    unique to forms

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Session.set('caretWatchConnectionId', Presences.findOne({_id:{$not:Meteor.connection._lastSessionId}})._id)
+
+### Example
+
+```html
+<form id="my-form" class="live-caret">
+  <input name="test" type="text">
+</form>
+```
--- live-caret.js
+++ live-caret.js
@@ -1,8 +1,11 @@
 $(function() {
   // keep track of our position and pass to our presence
-  $('input[type=text], textarea').bind('keyup click', function() {
+  $('form.live-caret input[type=text], form.live-caret textarea').bind('keyup click', function() {
+    var form = $(this).parents('form');
     var range = $(this).range();
+
     Session.set('caretRange', {
+      form: form.attr('id'),
       name: $(this).attr('name'),
       start: range.start,
       end: range.end,
@@ -36,8 +39,8 @@ Presence.state = function() {
   };
 };
 
-var setCaretPosition = function(name, coordinates) {
-  var field = $('[name=' + name + ']')[0];
+var setCaretPosition = function(form, fieldName, coordinates) {
+  var field = $('form#' + form + ' [name=' + fieldName + ']')[0];
   // var caret = $('[data-caret-name=' + name + ']');
   var caret = $('.caret');
   caret.css('display', 'block');
@@ -45,7 +48,7 @@ var setCaretPosition = function(name, coordinates) {
   caret.css('top', field.offsetTop - field.scrollTop + coordinates.top);
   caret.css('left', field.offsetLeft - field.scrollLeft + coordinates.left);
 
-  console.log(field, caret, coordinates);
+  console.log(caret, coordinates);
 };
 
 // get the caretRange from the presence, calculate the coordinates
@@ -55,13 +58,17 @@ var updateCaret = function(fields) {
   var coordinates = getCaretCoordinates($('[name=' + caretRange.name + ']')[0], position);
   console.log(position, coordinates);
 
-  setCaretPosition(caretRange.name, coordinates);
+  setCaretPosition(caretRange.form, caretRange.name, coordinates);
 };
 
 Meteor.startup(function() {
   Deps.autorun(function() {
     Presences.find(Session.get('caretWatchConnectionId')).observeChanges({
       added: function(id, fields) {
+        // sometimes this isn't yet set
+        if (! fields.state.caretRange)
+          return;
+
         updateCaret(fields);
       },
       changed: function(id, fields) {
